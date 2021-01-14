6b1c962a98842706de960974e5ec7a459e25e93c
joao pedro <joaof082@gmail.com>
2021-01-14 00:14:19 -0300

    clonando card com foto das pizzas

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(pizzaJson)
+const c = (el) => { return document.querySelector(el)};
+const cs = (el) => { return document.querySelector(el)}
+
+pizzaJson.map((item, index) => {
+  let pizzaItem = c(".models .pizza-item").cloneNode(true)
+  //preencher as informaçoes em pizza item
+
+  c('.pizza-area').append( pizzaItem );
+})
