8e8f13373b3b3048118b466ea9ba1366831f0cf1
Joao Pedro Ferreira <joaof082@gmail.com>
2021-01-19 19:15:58 -0300

    adicionando as pizzas na tela

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const c = (el) => { return document.querySelector(el)};
-const cs = (el) => { return document.querySelector(el)}
+const c = (el) => {return document.querySelector(el)};
+const cs = (el) => {return document.querySelector(el)};
 
-pizzaJson.map((item, index) => {
-  let pizzaItem = c(".models .pizza-item").cloneNode(true)
-  //preencher as informaçoes em pizza item
+pizzaJson.map((item,index) => {
+  let pizzaItem = c('.models .pizza-item').cloneNode(true);
+      // Clonando um Elmento da arvore DOM 
+  let cart = c(".models .cart--item").cloneNode
 
-  c('.pizza-area').append( pizzaItem );
-})
+  c('.pizza-area').append(pizzaItem);
+      // Atribuindo o PizzaItem A Pizza-area
+
+  
+}) 
