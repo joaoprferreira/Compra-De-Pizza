894a93ef6ef65935a32f97cd1b59870d3cc1b099
Joao Pedro Ferreira <joaof082@gmail.com>
2021-01-21 12:42:29 -0300

    adicionando imgens das pizzas

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const cs = (el) => {return document.querySelector(el)};
 pizzaJson.map((item,index) => {
   let pizzaItem = c('.models .pizza-item').cloneNode(true);
       // Clonando um Elmento da arvore DOM 
-  let cart = c(".models .cart--item").cloneNode
+    
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
   c('.pizza-area').append(pizzaItem);
       // Atribuindo o PizzaItem A Pizza-area
