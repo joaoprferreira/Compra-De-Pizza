0659c5a4bd77eda025467deea4a5c6c29a3fbbe6
Joao Pedro Ferreira <joaof082@gmail.com>
2021-01-21 21:55:43 -0300

    adicionadno nome e descrição nas pizzas do modal

--- script.js
+++ script.js
@@ -5,24 +5,30 @@ pizzaJson.map((item,index) => {
   let pizzaItem = c('.models .pizza-item').cloneNode(true);
       // Clonando um Elmento da arvore DOM 
     
+    pizzaItem.setAttribute('data-key', index);  
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
     pizzaItem.querySelector('a').addEventListener('click', (e) => { 
       e.preventDefault()
+      let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+      console.log(pizzaJson[key])
+
       let area = document.querySelector('.pizzaWindowArea');
       document.querySelector('.pizzaWindowArea').style.opacity = 0;
         area.style.display = "flex";
+
          setTimeout(() => {
           document.querySelector('.pizzaWindowArea').style.opacity = 1;
          },200);
-            document.querySelector('.pizzaBig img').src = item.img;
-              document.querySelector('.pizzaInfo h1').innerHTML = item.name
-                document.querySelector(".pizzaInfo--desc").innerHTML = item.description;
-                  document.querySelector('.pizzaInfo--size') = `${item.sizes}`;
+
+            c('.pizzaBig img').src = pizzaJson[key].img;
+            c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+            c(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
+            c('.pizzaInfo--size') = pizzaJson[key].sizes;
 
     })
 
